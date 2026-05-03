38baf91996ff07c4ed3d59b2a9a0e1ee402f8ff4
stuartstew <146310891+stuartstew@users.noreply.github.com>
2026-05-03 23:36:42 +0900

    feat: add header content (title & color scheme toggle)

--- src/features/header/components/header.tsx
+++ src/features/header/components/header.tsx
@@ -1,3 +1,11 @@
+import { Group, Title } from "@mantine/core";
+import { ColorSchemeToggle } from "./color-scheme-toggle";
+
 export const Header = () => {
-  return undefined;
+  return (
+    <Group h="100%" px="md" justify="space-between">
+      <Title order={3}>Color Picker</Title>
+      <ColorSchemeToggle />
+    </Group>
+  );
 };
