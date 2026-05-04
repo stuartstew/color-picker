ea0f695a32897ea880b412a3af4212717e4ffd11
stuartstew <146310891+stuartstew@users.noreply.github.com>
2026-05-04 18:19:01 +0900

    feat: add `AdjustedColorPicker`

--- src/features/main/components/main.tsx
+++ src/features/main/components/main.tsx
@@ -1,3 +1,15 @@
+import { Container, Group } from "@mantine/core";
+import { useRgb } from "../hooks/use-rgb";
+import { AdjustedColorPicker } from "./adjusted-color-picker";
+
 export const Main = () => {
-  return undefined;
+  const { hex, setHex } = useRgb({ r: 255, g: 0, b: 0 });
+
+  return (
+    <Container size={768} py={128}>
+      <Group justify="center" gap="xl">
+        <AdjustedColorPicker value={`#${hex}`} onChange={(value) => setHex(value.replace("#", ""))} />
+      </Group>
+    </Container>
+  );
 };
