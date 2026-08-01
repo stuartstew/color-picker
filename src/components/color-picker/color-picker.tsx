902249c29934bbaf2937ea13973aa49975992eb1
stuartstew <146310891+stuartstew@users.noreply.github.com>
2026-08-01 22:10:17 +0900

    feat: implement responsive style

--- src/components/color-picker/color-picker.tsx
+++ src/components/color-picker/color-picker.tsx
@@ -19,7 +19,7 @@ export const ColorPicker = () => {
 
   return (
     <Container size={theme.breakpoints.xs} mt={64} mb={32}>
-      <Group justify="center" h={224}>
+      <Group visibleFrom="xs" justify="center" h={224}>
         <HsvColorPicker value={hsv} onChange={changeHsv} />
         <Stack h="100%" justify="space-between" ml="xl">
           <ColorSwatch color={`#${hex}`} radius="md" w={224} h={36} />
@@ -35,6 +35,24 @@ export const ColorPicker = () => {
           </Group>
         </Stack>
       </Group>
+      <Stack hiddenFrom="xs" mx={16} gap="xl">
+        <HsvColorPicker value={hsv} onChange={changeHsv} />
+        <Group align="center" justify="space-between">
+          <ColorSwatch color={`#${hex}`} radius="md" h={160} flex={1} />
+          <Stack h="100%" w="12em" justify="space-between">
+            <Group gap="lg">
+              <RgbInput value={rgb} onChange={changeRgb} />
+              <HsvInput value={hsv} onChange={changeHsv} />
+            </Group>
+            <Group gap="xs">
+              <LeftLabel label="#" w="xs">
+                <HexInput size="xs" w={64} value={hex} onChange={setHex} />
+              </LeftLabel>
+              <CopyButton hex={hex} onCopy={addToHistory} />
+            </Group>
+          </Stack>
+        </Group>
+      </Stack>
       <Divider my="xl" />
       <ColorHistory colorHistory={colorHistory} onChangeHex={setHex} onDeleteItem={deleteItemFromHistory} />
     </Container>
